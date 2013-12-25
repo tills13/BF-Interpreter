17c258ffbb68eadce8dc6ecb9cdc83b6677facb6
Tyler Sebastian <tills13@gmail.com>
2013-12-24 22:21:27 -0800

    initial commit
    python interpreter for BrainFuck written in a drunk stupor

---
+++ bf.py
@@ -0,0 +1,58 @@
+import fileinput
+import sys
+import optparse
+import re
+
+index = 0
+cells = [0]
+def main():
+	p = optparse.OptionParser()
+	p.add_option("--file", dest="f", default = None)
+	#p.add_option("--verbose", dest="verbose", default = False)
+	options, arguments = p.parse_args()
+
+	mfile = open(options.f, 'r')
+	program = ''.join(mfile.readlines())
+	#verbose = options.verbose
+	parse(program, False)
+
+def parse(minput, verbose):
+	global index
+	global cells
+	pindex = 0
+
+	while(pindex < len(minput)):
+		if verbose:
+			print (minput[:pindex] + "*" + minput[pindex] + "*" + minput[pindex + 1:])
+			for i in range(len(cells)):
+				if (i == index): print("[%d]" % cells[i]),
+				else: print(" %d " % cells[i]),
+			print
+
+		char = minput[pindex]
+		if (char == '>'):
+			if (len(cells) <= (index + 1)):
+				cells.append(0)
+			index += 1
+		elif (char == '<'): index -= 1
+		elif (char == '.'): print(chr(cells[index])),
+		elif (char == ','): cells[index] = ord(sys.stdin.read(1))
+		elif (char == '['):
+			pindex += 1
+			lcount = 1
+			lend = pindex
+			while True:
+				#print lcount, minput[lend]
+				if minput[lend] == ']': lcount -= 1
+				elif minput[lend] == '[': lcount += 1
+				lend += 1
+				if (lcount == 0): break
+
+			if (cells[index] != 0): parse(minput[pindex:lend - 1], verbose)
+			else: pindex = lend + 1
+			pindex -= 2
+		elif (char == '-'): cells[index] -= 1
+		elif (char == '+'): cells[index] += 1
+		pindex += 1
+
+main()
